010f94c942de6543b605c38dfff7339423cba3be
Alkis Kastrisios <exalkas@yahoo.com>
2022-04-20 17:33:31 +0300

    notes added

---
+++ tasks.js
@@ -0,0 +1,13 @@
+/**
+ * To add a pod to the list of pods, use the following code:
+ * 
+ * kubctl apply -f <pod.yaml>
+ * 
+ * =============================
+ * To list the pods in the cluster and their status:
+ * 
+ * kubectl get pods
+ * =============================
+ * To list the services in the cluster:
+ * kubectl get services
+ */
