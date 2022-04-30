9ac61c81430d27ce285bf41308d864e652878075
alkas <rdxtbl@gmail.com>
2022-04-30 14:37:42 +0300

    db added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
-import 'express-async-errors';
+import 'express-async-errors'; // import this to don't get promises in custom errors with async functions
 import {} from 'dotenv/config';
+import mongoose from 'mongoose'
 
 import register from './routes/register'
 import { errorHandler } from './middlewares/errorHandler';
@@ -15,9 +16,20 @@ app.use(register)
 app.all('*', async (req, res, next) => {throw new NotFoundError()}) // handle all not found routes
 app.use(errorHandler)
 
+const startDB = async () => {
+
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Auth service, connected to DB')
+    } catch (error) {
+        console.log('Error connecting to DB:', error)
+    }
+}
+
+startDB()
+
 app.listen(5000, () => {
 
-    console.log('v2 running')
     console.log('Auth server is up and listens at port!!!', 5000)
 
 })
