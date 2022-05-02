f577d8f55e1770a08a29a9ac00117f6c537a86f6
alkas <rdxtbl@gmail.com>
2022-05-02 19:38:55 +0300

    login ip

--- auth/src/routes/register.ts
+++ auth/src/routes/register.ts
@@ -1,10 +1,10 @@
 import express, {Request, Response} from 'express'
 import jwt from 'jsonwebtoken'
-import {body, validationResult} from 'express-validator';
-import {RequestValidationError} from '../errors/requestValidationError';
+import {body} from 'express-validator';
 import { BadRequestError } from '../errors/badRequestError';
 import {User} from '../models/User';
 import {Password} from '../services/password';
+import validateRequest from '../middlewares/validateRequest';
 
 const router = express.Router();
 
@@ -14,16 +14,18 @@ router.post('/users/register', [
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
-  ], async (req: Request, res: Response) => {
+  ],
+  validateRequest, 
+  async (req: Request, res: Response) => {
 
-    const errors = validationResult(req);
+    // const errors = validationResult(req);
 
-    if (!errors.isEmpty()) { // if there are errors
+    // if (!errors.isEmpty()) { // if there are errors
         
-        throw new RequestValidationError(errors.array());
-        // throw new Error('Invalid email or password');
-        // return  res.status(400).send(errors.array()); // send the errors in array format
-    }
+    //     throw new RequestValidationError(errors.array());
+    //     // throw new Error('Invalid email or password');
+    //     // return  res.status(400).send(errors.array()); // send the errors in array format
+    // }
 
     Password.hashPassword('string')
     const {email, pass} = req.body;
